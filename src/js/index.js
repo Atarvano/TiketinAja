92fdee61ddf362c885b674dc43d3b4d2f40daa6e
Timothy William <timothycod3r@gmail.com>
2025-04-22 11:42:23 +0700

    tmbahin icon

--- src/js/index.js
+++ src/js/index.js
@@ -2,3 +2,7 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
 import "bootstrap";
+import "../css/style.css";
+import 'boxicons';
+import "../css/pricing.css";
+
